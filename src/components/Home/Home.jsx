8ef5d3b0d1d25fe4d5a8d1592f03b0521925a598
danieskka <daniescalera93@hotmail.com>
2023-06-29 15:35:57 +0200

    add/handleOnChange

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+const  Home = () => {
+
+    const [value, setValue] = useState({
+      text: ''
+    });
+
+    const handleChange = (e) => {
+      setValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log(value)
+    }
+
+  return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="text">Escribe algo</label>
+          <input name ="text" type="text" onChange={handleChange}></input>
+          <br/>
+          <button>ADD</button>
+        </form>
+        <p>{JSON.stringify(value)}</p>
+      </>
+  )
 };
 
 export default Home;
